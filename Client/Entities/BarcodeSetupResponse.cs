3339a5e372de71a9c59077c9f80bba8e1267a67d
Tim Vynckier <tvynckier@conxion.be>
2023-11-28 10:45:19 +0100

    add code

---
+++ Client/Entities/BarcodeSetupResponse.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace BarcodeScanner.Entities
+{
+    public class BarcodeSetupResponse
+    {
+        [JsonProperty("@odata.context")]
+        public string odatacontext { get; set; }
+        public List<Value> value { get; set; }
+    }
+
+    public class Value
+    {
+        [JsonProperty("@odata.etag")]
+        public string odataetag { get; set; }
+        public string id { get; set; }
+        public string worksheetTemplateName { get; set; }
+        public string journalBatchName { get; set; }
+    }
+}
+
---
+++ Client/Entities/ReqLine.cs
@@ -0,0 +1,11 @@
+﻿namespace BarcodeScanner.Entities
+{
+    public class ReqLine
+    {
+        public string number { get; set; }
+        public string worksheetTemplateName { get; set; }
+        public string journalBatchName { get; set; }
+        public string Type { get; set; }
+        public decimal quantity { get; set; }
+    }
+}
